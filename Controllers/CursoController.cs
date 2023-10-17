f0ec6f6623bd25d1e76ecad857407b77813543c8
GaleguinhoBR <isaac.vilton@escolar.ifrn.edu.br>
2023-10-17 11:52:35 -0300

    Quarto Commit

--- Controllers/CursoController.cs
+++ Controllers/CursoController.cs
@@ -29,6 +29,13 @@ namespace apiUniversidade.Controllers
             
             return cursos;
         }
+        [HttpPost]
+        public ActionResult  Post(Curso curso){
+            _context.Cursos.Add(curso);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("GetCurso", new{id = curso.Id}, curso);
+        }
     } 
 
 }
