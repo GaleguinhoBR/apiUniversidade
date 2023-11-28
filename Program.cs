805078c7e67090942bb3e243fa305186e3bf9cdd
GaleguinhoBR <isaac.vilton@escolar.ifrn.edu.br>
2023-11-28 11:04:58 -0300

    Sétimo Commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using apiUniversidade.Context;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,9 @@ builder.Services.AddDbContext<apiUniversidadeContext>(options => options.UseNpgs
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddIdentity<IdentityUser,IdentityRole>()
+    .AddEntityFrameworkStores<apiUniversidadeContext>()
+    .AddDefaultTokenProviders();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
