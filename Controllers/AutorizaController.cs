45d31d5807da11c26f4971b44ff6be54589b7077
GaleguinhoBR <isaac.vilton@escolar.ifrn.edu.br>
2023-12-05 11:59:38 -0300

    Nono Commit

---
+++ Controllers/AutorizaController.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using apiUniversidade.DTO;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+namespace apiUniversidade.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AutorizaController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IConfiguration _configuration;
+
+        public AutorizaController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "AutorizaController :: Acessado em : " + DateTime.Now.ToLongDateString();
+        }
+        [HttpPost("register")]
+        public async Task<ActionResult> RegisterUser([FromBody] UsuarioDTO model)
+        {
+            var user = new IdentityUser{
+                UserName = model.Email,
+                Email = model.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if(!result.Succeeded)
+                return BadRequest(result.Errors);
+            
+            await _signInManager.SignInAsync(user, false);
+            //return Ok(GerarToken(model));
+            return Ok();
+        }
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] UsuarioDTO userInfo){
+            var result = await _signInManager.PasswordSignInAsync(userInfo.Email, userInfo.Password, isPersistent: false, lockoutOnFailure: false);
+            if(result.Succeeded)
+                return Ok();
+            else{
+                ModelState.AddModelError(string.Empty,"Login Inválido...");
+                return BadRequest(ModelState);
+            }
+        }
+        private UsuarioToken GeraToken(UsuarioDTO userinfo){
+            var claims = new[]{
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.UniqueName,userinfo.Email),
+                new Claim("IFRN","TecInfo"),
+                new Claim(Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricAlgorithmSecurityKey(Encooding.UTF8.GetBytes(_configuration["Jwt:key"]));
+
+            var credentials = new SigningCredentials(key,SecurityAlgorithms.Hmac5ha256);
+
+            var expiracao = _configuration["TokenConfiguration:ExpireHours"];
+            var expiration = DateTime.UtcNow.AddHours(double.Parse(expiracao));
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer: _configuration["TokenConfiguration:Issuer"],
+                audience: _configuration["TokenConfiguration:Audience"],
+                claims: claims,
+                expires: expiration,
+                signingCredentials: credentials
+            );
+
+            return new UsuarioToken(){
+                Authenticated = true,
+                Expiration = expiration,
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Message = "JWT Ok."
+            };
+        }
+    }
+}
